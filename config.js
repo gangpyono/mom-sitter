9e9aa76c5790fbec10fd79c2393a22de8d7b1e52
shrkdvy <shrkdvy123@gmail.com>
2022-02-18 12:55:19 +0900

    최종 수정

--- config.js
+++
@@ -1,3 +0,0 @@
-export const config = {
-  auth: `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`,
-};
--- src/apis/index.js
+++ src/apis/index.js
@@ -1,10 +1,8 @@
 import axios from "axios";
-import { config } from "../../config";
 
 export const fetcher = axios.create({
   baseURL: "https://api.github.com",
   headers: {
-    authorization: config.auth,
     Accept: "application/vnd.github.v3+json",
   },
 });
